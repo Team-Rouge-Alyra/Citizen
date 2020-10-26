b5454c27654a6beb9c2daa9431131078bf937b7a
Sourak135 <65900864+Sourak135@users.noreply.github.com>
2020-10-26 23:58:40 +0100

    Add files via upload
    candidature sage + vote sage + result ok - return ne fonctionnent pas

--- Citizen.sol
+++ Citizen.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
 
 contract Citizen{
+    
+//structure des citoyens        
     struct Citoyen{
         bool citoyen; // a reflechir
-        uint256 id;
+        uint256 id; // a remplacer par name?
         uint256 wallet; // argent possédé
         bool travailleur;
         bool chomeur;
@@ -14,24 +16,32 @@ contract Citizen{
         uint256 cotisation_maladie;
         bool retraite;
         uint256 cotisation_retraite;
-        uint8 age; // a remplir lors register pour prevoir la retraite
+        uint256 age; // a remplir lors register pour prevoir la retraite
         bool banni; //banni 10 ans?
         //bool mort; //pas sur utile et trop de lignes pour que ça passe avec
 
     }
-    struct Sage{
-        bool sage;
-        uint256 depot_sage;
-        uint8 elu; //sage pdt 8 semaines =  block.timestamp + 8 weeks
-    }
     
+// strucuture des sages - trop de lignes si intégré a Citoyen
+    struct Sage{
+        bool isSage;
+        uint256 depot_sage; // 100 Citizen
+        uint256 delayRegistration; //sage pdt 8 semaines =  block.timestamp + 8 weeks
+    }
+
+//structure des entreprises    
     struct Entreprises{
         uint256 id; // siret
         uint256 argent;
         bool statut; //statut valide?
     }
-    
-    uint256 public _id;
+
+
+// id pour ne pas avoir deux citoyens avec id identiques    
+    uint256 private _id;
+// compteur pour identifier vote
+    uint256 private compteur;
+//budget, incrémenté par impots, amendes, décès; décrémenté par nouveaux citoyens et entreprises   
     uint256 public budget;
     string public help_gestion_status = "change le status 0=travailleur, 1=chomeur, 2=malade, 3=mort";
     enum statut { travailleur, chomeur, malade, mort }
@@ -40,27 +50,50 @@ contract Citizen{
     mapping (address => Sage ) public admin;
     mapping (address => Entreprises ) public travail;
     
+    //a débattre en team, solution pour 1er citoyen / 1er sage. 5200 weeks car si plus de sage on est mals - pour l'instant nécessaire pour faire mes tests.
+    constructor(uint8 _age) public {
+        admin[msg.sender].isSage = true;
+        admin[msg.sender].depot_sage = 100;
+        admin[msg.sender].delayRegistration = block.timestamp + 5200 weeks;
+        administration[msg.sender].citoyen = true;
+        administration[msg.sender].id = 0;
+        administration[msg.sender].age=_age;
+    }
+
+    modifier onlySage (){
+            require (admin[msg.sender].isSage == true, "only Sage can do this");
+            require(admin[msg.sender].delayRegistration > block.timestamp, "vous n'êtes plus sage");
+            _;
+    }
+     
+     modifier onlyCitoyen (){
+            require (administration[msg.sender].citoyen == true, "fonction réservée aux citoyens");
+            _;
+    }   
     
-    function register(address _addr) public {
-        require (administration[_addr].citoyen==false, "vous êtes déjà citoyen");
+// enregistrement de nouveau citoyen, vérif pas déjà citoyen, attribue citoyenneté, id, 100 citizen (retirés au budget), incrémente id général     
+    function register(address _addr, uint256 _age) public onlySage{
+        require (administration[_addr].citoyen==false, "cette personne est déjà citoyen");
+        
         administration[_addr].citoyen=true;
+        administration[_addr].age =(_age*52);
+        administration[_addr].wallet+=100;
+        budget-=100;
         administration[_addr].id=_id+1;
-        administration[_addr].wallet=100;
         _id+=1;
+        
     }
     
-    
-    function gestion_statut(statut _ide, address _addr) public {
+//gestion de statut par sage  0 travailleur, 1 chomeur, 2 malade, 3 mort     
+    function gestion_statut(statut _ide, address _addr) public onlySage {
         if (_ide==statut.travailleur){
             administration[_addr].travailleur?administration[_addr].travailleur=false:administration[_addr].travailleur=true;
         }
         else if (_ide==statut.chomeur){
             administration[_addr].chomeur?administration[_addr].chomeur=false:administration[_addr].chomeur=true;
-            
         }
         else if (_ide==statut.malade){
             administration[_addr].malade?administration[_addr].malade=false:administration[_addr].malade=true;
-            
         }
         else if (_ide==statut.mort){
             budget+=administration[_addr].wallet;
@@ -73,6 +106,66 @@ contract Citizen{
             administration[_addr].cotisation_retraite=0;
         }
     }
+    
+// 
+// ajout du  code ci dessous + constructeur l 53 + modifier l69
+//
+    struct Candidat{
+        uint256 id;
+        address candidat;//
+        uint256 temps_vote;
+        uint256 voteOui;
+        uint256 voteNon;
+        mapping (address => bool) didVote;
+    }
+    
+    mapping (uint256 => Candidat) public a_voter;
+    
+    enum choix {Oui, Non}
+
+    // 2 minutes au lieu de 1 weeks pour les tests - comprend pas pourquoi les return ne fonctionnent pas 
+    function se_presenter_sage()public onlyCitoyen returns (string memory, uint256, string memory){
+        require (administration[msg.sender].wallet>=100, "vous ne possédez pas la somme a mettre en dépot pour vous présenter");
+        require (admin[msg.sender].isSage==false);
+        compteur +=1;
+        a_voter[compteur].id=compteur;
+        a_voter[compteur].temps_vote = block.timestamp + 2 minutes;
+        return ("le vote", a_voter[compteur].id, "est en cours");
+    }
+    
+
+    function vote(uint _nbvote, choix bulletin) public onlyCitoyen {
+        require ((block.timestamp)<(a_voter[_nbvote].temps_vote), "ce vote est terminé");
+        require (a_voter[_nbvote].didVote[msg.sender]==false, "vous avez déjà voté");
+        if (bulletin == choix.Oui){
+            a_voter[_nbvote].voteOui+=1;
+            a_voter[_nbvote].didVote[msg.sender]=true;
+        }
+        else if (bulletin == choix.Non){
+            a_voter[_nbvote].voteNon+=1;
+            a_voter[_nbvote].didVote[msg.sender]=true;
+        }
+        else {revert("choix pour le vote incompris");
+            
+        }
+    }
+    
+    //comprend pas pourquoi les return ne fonctionnent pas 
+    function result(uint _nbvote) public returns(string memory, uint256, uint256){
+        require ((block.timestamp)>(a_voter[_nbvote].temps_vote), "ce vote est en cours");
+        if(a_voter[_nbvote].voteOui>a_voter[_nbvote].voteNon){
+            admin[(a_voter[_nbvote].candidat)].isSage=true;
+            admin[(a_voter[_nbvote].candidat)].depot_sage+=100;
+            administration[(a_voter[_nbvote].candidat)].wallet-=100;
+            admin[(a_voter[_nbvote].candidat)].delayRegistration=block.timestamp + 8 weeks;
+            return ("le candidat a été élu", a_voter[_nbvote].voteOui, a_voter[_nbvote].voteNon);
+        }
+        else {
+            return ("le candidat n'a pas été élu", a_voter[_nbvote].voteOui, a_voter[_nbvote].voteNon);
+        }
+    }
+    
+    
     
 }
     
